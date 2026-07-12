f6fffaa8951c62fe223b7d4ba46b1eb32b97289c
dani_ <daniinad175.may30@gmail.com>
2026-07-12 19:58:36 -0300

    correccion de error

--- sudoku-app/src/components/icons.jsx
+++ sudoku-app/src/components/icons.jsx
@@ -99,3 +99,13 @@ export function NotesIcon({ size = 20, ...props }) {
     </Icon>
   );
 }
+
+export function LeaderboardIcon({ size = 16, ...props }) {
+  return (
+    <Icon size={size} viewBox="0 0 24 24" {...props}>
+      <line x1="18" y1="20" x2="18" y2="10" />
+      <line x1="12" y1="20" x2="12" y2="4" />
+      <line x1="6" y1="20" x2="6" y2="14" />
+    </Icon>
+  );
+}
